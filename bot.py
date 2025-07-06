9ded1a2eb5b256bd8db5996918c3ee1ca892bbdc
falbue <cyansair05@gmail.com>
2025-07-06 23:50:08 +0500

    Вывод пути базы

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
     load_dotenv()
     TOKEN = os.getenv("BOT_TOKEN")
     DATABASE = os.getenv("DATABASE")
+    print(database)
     print(f"Токен: {TOKEN}")
     DEBUG = os.getenv("DEBUG", "false").lower() in ("true", "1", "yes")
     TelegramTextApp.start(TOKEN, "bot.json", DATABASE, debug=DEBUG)

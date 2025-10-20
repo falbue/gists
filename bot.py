f2030dc8163f99b8161d6dc44986f70afb5570eb
falbue <cyansair05@gmail.com>
2025-10-20 19:30:37 +0500

    zed сделал pep8

--- bot.py
+++ bot.py
@@ -7,20 +7,25 @@ import os
 
 
 def create_tokens():
-    SQL_request('''
+    SQL_request("""
     CREATE TABLE IF NOT EXISTS github_tokens (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         user_id INTEGER NOT NULL,
         token TEXT,
         FOREIGN KEY (user_id) REFERENCES TTA(id)
-    )''')
+    )""")
+
 
 def get_token(tta_data):
     user_id = tta_data["id"]
-    result = SQL_request("SELECT name FROM sqlite_master WHERE type='table' AND name='github_tokens';")
+    result = SQL_request(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='github_tokens';"
+    )
     if not result:
         create_tokens()
-    token_data = SQL_request("SELECT * FROM github_tokens WHERE user_id=?", (user_id,), 'one')
+    token_data = SQL_request(
+        "SELECT * FROM github_tokens WHERE user_id=?", (user_id,), "one"
+    )
     if token_data:
         return token_data["token"]
     else:
@@ -36,11 +41,14 @@ def fetch_github_data(url, tta_data):
     response.raise_for_status()
     return response.json()
 
+
 def gist_data(tta_data):
     try:
-        gist_id = fetch_github_data("https://api.github.com/gists", tta_data)[int(tta_data["gist_id"])]["id"]
+        gist_id = fetch_github_data("https://api.github.com/gists", tta_data)[
+            int(tta_data["gist_id"])
+        ]["id"]
         gist = fetch_github_data(f"https://api.github.com/gists/{gist_id}", tta_data)
-        
+
         return {
             "gist_description": gist.get("description", ""),
             "gist_num_files": len(gist["files"]),
@@ -48,37 +56,44 @@ def gist_data(tta_data):
             "gist_forks_count": len(gist.get("forks", [])),
             "gist_comments": gist["comments"],
             "gist_created_at": gist["created_at"],
-            "gist_owner": f"https://github.com/users/{gist['owner']['login']}"
+            "gist_owner": f"https://github.com/users/{gist['owner']['login']}",
         }
     except Exception as e:
         print(f"Возникла ошибка: {e}")
         return {"{gist_description}": f"GitHub API error: {str(e)}"}
 
-def get_gists(tta_data):    
+
+def get_gists(tta_data):
     try:
         gists = fetch_github_data("https://api.github.com/gists", tta_data)
-        data = {f'gist|{index}': g["description"] for index, g in enumerate(gists)}
+        data = {f"gist|{index}": g["description"] for index, g in enumerate(gists)}
         if not data:
             return {"none_gist": "А где?"}
         return data
     except:
         return {"add_token": "Добавить токен авторизации"}
 
-def gist_files(tta_data):  #список файлов в гисте
+
+def gist_files(tta_data):  # список файлов в гисте
     try:
         gist_index = tta_data["gist_id"]
-        gist_id = fetch_github_data("https://api.github.com/gists", tta_data)[int(tta_data["gist_id"])]["id"]
+        gist_id = fetch_github_data("https://api.github.com/gists", tta_data)[
+            int(tta_data["gist_id"])
+        ]["id"]
         gist = fetch_github_data(f"https://api.github.com/gists/{gist_id}", tta_data)
         return {
-            f'gist_file|{gist_index}|{file["filename"]}': file["filename"]
+            f"gist_file|{gist_index}|{file['filename']}": file["filename"]
             for file in gist["files"].values()
         }
     except requests.exceptions.RequestException as e:
         return {"error": f"Ошибка при получении файлов: {str(e)}"}
 
-def code_file(tta_data):  #содержимое файла из гиста
+
+def code_file(tta_data):  # содержимое файла из гиста
     try:
-        gist_id = fetch_github_data("https://api.github.com/gists", tta_data)[int(tta_data["gist_id"])]["id"]
+        gist_id = fetch_github_data("https://api.github.com/gists", tta_data)[
+            int(tta_data["gist_id"])
+        ]["id"]
         gist = fetch_github_data(f"https://api.github.com/gists/{gist_id}", tta_data)
         file_data = gist["files"].get(tta_data["gist_file"])
         if not file_data:
@@ -88,26 +103,38 @@ def code_file(tta_data):  #содержимое файла из гиста
         print(f"Ошибка при открытии файла: {e}")
         return {"code": f"Ошибка при получении кода: {str(e)}"}
 
+
 def save_token(tta_data):
     user_id = tta_data["id"]
     token = tta_data.get("github_token")
     if token:
-        token_data = SQL_request("SELECT * FROM github_tokens WHERE user_id=?", (user_id,), 'one')
+        token_data = SQL_request(
+            "SELECT * FROM github_tokens WHERE user_id=?", (user_id,), "one"
+        )
         if token_data:
-            SQL_request("UPDATE github_tokens SET token = ? WHERE user_id = ?", (token, user_id))
+            SQL_request(
+                "UPDATE github_tokens SET token = ? WHERE user_id = ?", (token, user_id)
+            )
         else:
-            SQL_request('INSERT INTO github_tokens (user_id, token) VALUES (?, ?)', (user_id, token))
+            SQL_request(
+                "INSERT INTO github_tokens (user_id, token) VALUES (?, ?)",
+                (user_id, token),
+            )
             return
     else:
-        return {"error_text":"Не верный токен"}
+        return {"error_text": "Не верный токен"}
+
 
 def get_user_token(tta_data):
     user_id = tta_data["id"]
-    token = SQL_request("SELECT token FROM github_tokens WHERE user_id=?", (user_id,), 'one')
+    token = SQL_request(
+        "SELECT token FROM github_tokens WHERE user_id=?", (user_id,), "one"
+    )
     if not token:
-        return {"token":"Токен не найден"}
+        return {"token": "Токен не найден"}
     else:
-        return {"token":f"`{token['token']}`"}
+        return {"token": f"`{token['token']}`"}
+
 
 if __name__ == "__main__":
     TelegramTextApp.start()
